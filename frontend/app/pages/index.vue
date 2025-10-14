b27e573d72d09cbdecbf3265143b6af25ce07ac8
amirulabdlatib <amirulal73@gmail.com>
2025-10-14 12:51:32 +0800

    Add tailwind css

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,1 +1,3 @@
-<template>hello world</template>
+<template>
+    <h3 class="underline">hello world</h3>
+</template>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,8 +1,15 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: "2025-07-15",
     devtools: { enabled: true },
     modules: ["nuxt-auth-sanctum"],
+    css: ["~/assets/css/main.css"],
+
+    vite: {
+        plugins: [tailwindcss()],
+    },
 
     sanctum: {
         baseUrl: process.env.NUXT_BACKEND_API_URL,
