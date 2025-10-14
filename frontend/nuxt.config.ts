56eb84484cbc7454e5bf72094fbf58e5ad2527a2
amirulabdlatib <amirulal73@gmail.com>
2025-10-14 12:01:56 +0800

    Add nuxt sanctum module

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+    compatibilityDate: "2025-07-15",
+    devtools: { enabled: true },
+    modules: ["nuxt-auth-sanctum"],
+
+    sanctum: {
+        baseUrl: "http://localhost:8000",
+    },
+});
