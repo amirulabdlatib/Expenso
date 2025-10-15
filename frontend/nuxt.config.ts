f64432e008300b55d793c6c1992ad3585576064a
amirulabdlatib <amirulal73@gmail.com>
2025-10-15 15:36:34 +0800

    Add nuxt icon

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
     compatibilityDate: "2025-07-15",
     devtools: { enabled: true },
-    modules: ["nuxt-auth-sanctum", "@nuxtjs/google-fonts"],
+    modules: ["nuxt-auth-sanctum", "@nuxtjs/google-fonts", "@nuxt/icon"],
     css: ["~/assets/css/main.css"],
 
     vite: {
