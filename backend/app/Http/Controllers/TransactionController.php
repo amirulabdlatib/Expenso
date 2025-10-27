34f9c7ac033884088057ffd9ca84173faf5971f1
amirulabdlatib <amirulal73@gmail.com>
2025-10-27 09:08:43 +0800

    Reformat on transaction controller

--- backend/app/Http/Controllers/TransactionController.php
+++ backend/app/Http/Controllers/TransactionController.php
@@ -14,18 +14,18 @@ class TransactionController extends Controller
      */
     public function index()
     {
-        $transactions = Transaction::where('user_id',Auth::id())
-                            ->with([
-                                'accounts:name,icon',
-                                'categories:name,icon,color,type'
-                            ])->get();
-        
+        $transactions = Transaction::where('user_id', Auth::id())
+            ->with([
+                'accounts:name,icon',
+                'categories:name,icon,color,type'
+            ])->get();
+
         $total_income = Transaction::totalIncome();
         $total_expenses = Transaction::totalExpenses();
-        $total_transaction_this_month = Transaction::where('user_id',Auth::id())
-                                                    ->whereMonth('created_at', Carbon::now()->month)
-                                                    ->whereYear('created_at', Carbon::now()->year)
-                                                    ->count();
+        $total_transaction_this_month = Transaction::where('user_id', Auth::id())
+            ->whereMonth('created_at', Carbon::now()->month)
+            ->whereYear('created_at', Carbon::now()->year)
+            ->count();
 
         return response()->json([
             'transactions' => $transactions,
