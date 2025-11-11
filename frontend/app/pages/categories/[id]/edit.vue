1ec51ad70182c489f1568c522789a3a541ead283
amirulabdlatib <amirulal73@gmail.com>
2025-11-11 09:08:25 +0800

    Add show and edit page FE account page

---
+++ frontend/app/pages/categories/[id]/edit.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>edit page</div>
+</template>
+<script setup>
+    useHead({
+        title: "Account Details - Expenso",
+    });
+    definePageMeta({
+        middleware: ["sanctum:auth"],
+    });
+</script>
---
+++ frontend/app/pages/categories/[id]/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>detail category page</div>
+</template>
+
+<script setup>
+    useHead({
+        title: "Account Details - Expenso",
+    });
+    definePageMeta({
+        middleware: ["sanctum:auth"],
+    });
+</script>
