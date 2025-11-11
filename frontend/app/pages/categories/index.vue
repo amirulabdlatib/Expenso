04ed3c6326e55730c8241a3197105eb417193658
amirulabdlatib <amirulal73@gmail.com>
2025-11-11 16:57:17 +0800

    Update icon on category card index page

--- frontend/app/pages/categories/index.vue
+++ frontend/app/pages/categories/index.vue
@@ -85,7 +85,7 @@
                         </div>
                         <div class="flex items-center space-x-2">
                             <NuxtLink :to="`/categories/${category.id}`" class="p-2 text-gray-400 hover:text-indigo-600 rounded-lg hover:bg-indigo-50 transition-colors">
-                                <Icon name="heroicons:pencil" class="w-4 h-4" />
+                                <Icon name="heroicons:eye" class="w-4 h-4" />
                             </NuxtLink>
                             <button class="p-2 text-gray-400 hover:text-red-600 rounded-lg hover:bg-red-50 transition-colors" @click="handleDelete(category.id)">
                                 <Icon name="heroicons:trash" class="w-4 h-4" />
